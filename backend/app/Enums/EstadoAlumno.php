b49306e2ca17aaff7be18d9b27cab85f29c912fe
maximofacha <max42582092@gmail.com>
2026-04-22 23:05:39 -0300

    fix: restaurar todos los valores del enum EstadoAlumno para compatibilidad con datos existentes

--- backend/app/Enums/EstadoAlumno.php
+++ backend/app/Enums/EstadoAlumno.php
@@ -4,18 +4,24 @@ namespace App\Enums;
 
 enum EstadoAlumno: string
 {
-    case Regular    = 'regular';
-    case Egresado   = 'egresado';
-    case Abandono   = 'abandono';
-    case Suspendido = 'suspendido';
+    case Regular      = 'regular';
+    case Egresado     = 'egresado';
+    case Abandono     = 'abandono';
+    case Suspendido   = 'suspendido';
+    case Preinscripto = 'preinscripto';
+    case Sorteado     = 'sorteado';
+    case Inscripto    = 'inscripto';
 
     public function label(): string
     {
         return match($this) {
-            self::Regular    => 'Regular',
-            self::Egresado   => 'Egresado',
-            self::Abandono   => 'Abandono',
-            self::Suspendido => 'Suspendido',
+            self::Regular      => 'Regular',
+            self::Egresado     => 'Egresado',
+            self::Abandono     => 'Abandono',
+            self::Suspendido   => 'Suspendido',
+            self::Preinscripto => 'Preinscripto',
+            self::Sorteado     => 'Sorteado',
+            self::Inscripto    => 'Inscripto',
         };
     }
 }
